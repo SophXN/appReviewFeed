260d9df97fba7b18d7a95174bd253993c3b404f2
Sophia Nguyen <nguyen.x.sophia@gmail.com>
2021-11-19 15:27:35 -0800

    Script Complete, currently showing Reviews for Facebook

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"time"
 )
 
+type Reviews struct {
+	Reviewer string
+	Date     string
+	Score    string
+	Content  string
+}
+
 type Feeds struct {
 	Feeds Feed `json:"feed"`
 }
@@ -24,7 +31,7 @@ type Entry struct {
 		} `json:"name"`
 	} `json:"author"`
 	DateLabel struct {
-		Date string `json:"label"`
+		Date time.Time `json:"label"`
 	} `json:"updated"`
 	ContentLabel struct {
 		Content string `json:"label"`
@@ -35,7 +42,9 @@ type Entry struct {
 }
 
 func main() {
-	doEvery(20000*time.Millisecond, getJson)
+	fmt.Printf("Ello Mate! You must be looking for new app store reviews!\nLet me go check on that for you....\n\n")
+	art()
+	doEvery(5000*time.Millisecond, getJson)
 }
 
 func doEvery(d time.Duration, f func(time.Time)) {
@@ -45,34 +54,29 @@ func doEvery(d time.Duration, f func(time.Time)) {
 }
 
 func getJson(t time.Time) {
-	response, err := http.Get("https://itunes.apple.com/us/rss/customerreviews/id=595068606/sortBy=mostRecent/page=1/json")
+	response, err := http.Get("https://itunes.apple.com/us/rss/customerreviews/id=284882215/sortBy=mostRecent/page=1/json")
 	if err != nil {
-		fmt.Printf("No works with error %s\n", err)
+		fmt.Println(err)
+
 	} else {
-		parseJson("review.json")
 		data, _ := ioutil.ReadAll(response.Body)
 		err := ioutil.WriteFile("review.json", data, 0777)
-		fmt.Printf("\nCreated a new file at: %v", t)
+		parseJson("review.json")
+		e := os.Remove("review.json")
+		if e != nil {
+			fmt.Println(e)
+		}
 		if err != nil {
 			fmt.Println(err)
 		}
 	}
 }
 
-type Reviews struct {
-	Reviewer string
-	Date     string
-	Score    string
-	Content  string
-}
-
 func parseJson(f string) {
-
 	jsonFile, err := os.Open(f)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("Successfully Opened %v", f)
 
 	defer jsonFile.Close()
 	byteValue, _ := ioutil.ReadAll(jsonFile)
@@ -82,16 +86,69 @@ func parseJson(f string) {
 	newReviewList := make([]Reviews, 0, 10)
 
 	for i := 0; i < len(review.Feeds.Entries); i++ {
-		newReview := Reviews{
-			(review.Feeds.Entries[i].Reviewer.NameLabel.Name),
-			(review.Feeds.Entries[i].DateLabel.Date),
-			(review.Feeds.Entries[i].ScoreLabel.Score),
-			(review.Feeds.Entries[i].ContentLabel.Content),
+		checkReview := CheckReview(review.Feeds.Entries[i].DateLabel.Date)
+
+		if checkReview {
+			convTime := ConvertTime(review.Feeds.Entries[i].DateLabel.Date)
+			newReview := Reviews{
+				review.Feeds.Entries[i].Reviewer.NameLabel.Name,
+				convTime.Format("2006-01-02 15:04:00"),
+				review.Feeds.Entries[i].ScoreLabel.Score,
+				review.Feeds.Entries[i].ContentLabel.Content,
+			}
+			newReviewList = append(newReviewList, newReview)
+		} else {
+			continue
 		}
-		newReviewList = append(newReviewList, newReview)
 	}
 
+	name := GetFilenameDate()
 	file, _ := json.MarshalIndent(newReviewList, "", " ")
+	_ = ioutil.WriteFile(name, file, 0644)
 
-	_ = ioutil.WriteFile("output.json", file, 0644)
+	currentTime := time.Now()
+	yest := currentTime.Add(-time.Hour * 24)
+	count := len(newReviewList)
+	fmt.Printf("\nWOW There are %d New reviews since: %s\n", count, yest.Format("2006-01-02 15:04:00"))
+	fmt.Printf("\nThey are saved in %s for you.", name)
+}
+
+func ConvertTime(t time.Time) time.Time {
+	loc, err := time.LoadLocation("America/Los_Angeles")
+	localStartTime := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), loc)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return localStartTime
+}
+
+func GetFilenameDate() string {
+	const layout = "01-02-2006"
+	t := time.Now()
+
+	return t.Format(layout) + ".json"
+}
+
+func CheckReview(t time.Time) bool {
+	currentTime := time.Now()
+	yest := currentTime.Add(-time.Hour * 48)
+
+	if t.After(yest) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func art() {
+	hehe := `
+	██████╗ ██╗   ██╗███╗   ██╗██╗    ██╗ █████╗ ██╗   ██╗
+	██╔══██╗██║   ██║████╗  ██║██║    ██║██╔══██╗╚██╗ ██╔╝
+	██████╔╝██║   ██║██╔██╗ ██║██║ █╗ ██║███████║ ╚████╔╝
+	██╔══██╗██║   ██║██║╚██╗██║██║███╗██║██╔══██║  ╚██╔╝
+	██║  ██║╚██████╔╝██║ ╚████║╚███╔███╔╝██║  ██║   ██║
+	╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝
+	`
+	fmt.Println(hehe)
 }
